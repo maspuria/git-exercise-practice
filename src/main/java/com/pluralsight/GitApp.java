b20264a024d25896649e7d450ce5ed51d15b6c54
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-26 12:52:10 -0600

    add verification feature

--- src/main/java/com/pluralsight/GitApp.java
+++ src/main/java/com/pluralsight/GitApp.java
@@ -3,17 +3,21 @@ package com.pluralsight;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("Let's Practice Git Bash Terminal Usage");
-        System.out.println("==== Set Up Project ====");
-        System.out.println("- git init");
-        System.out.println("- git add . ");
-        System.out.println("- git commit -m 'commit message inside double quotes' ");
-        System.out.println("- git remote add origin [url]");
-        System.out.println("- git push origin main");
+        System.out.println("/n ==== Set Up Project ====");
+        System.out.println(" - git init");
+        System.out.println(" - git add . ");
+        System.out.println(" - git commit -m 'commit message inside double quotes' ");
+        System.out.println(" - git remote add origin [url]");
+        System.out.println(" - git push origin main");
 
-        System.out.println("=== Ongoing Workflow === ");
+        System.out.println("/n === Ongoing Workflow === ");
         System.out.println("update (edit, add, or delete) code");
         System.out.println(" - git add . ");
         System.out.println(" - git commit -m 'commit message inside double quotes' ");
         System.out.println(" - git push origin main");
+
+        System.out.println("/n === Verification ===");
+        System.out.println(" - git status");
+        System.out.println(" - git log --oneline ");
     }
 }
