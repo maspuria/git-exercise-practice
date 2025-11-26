b1ecacba1d9ec79ea33ff74914fb6447049b7816
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-26 11:05:13 -0600

    add set up project notes

--- src/main/java/com/pluralsight/GitApp.java
+++ src/main/java/com/pluralsight/GitApp.java
@@ -3,5 +3,11 @@ package com.pluralsight;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("Let's Practice Git Bash Terminal Usage");
+        System.out.println("==== Set Up Project ====");
+        System.out.println("- git init");
+        System.out.println("- git add . ");
+        System.out.println("- git commit -m 'commit message inside double quotes' ");
+        System.out.println("- git remote add origin [url]");
+        System.out.println("- git push origin main");
     }
 }
